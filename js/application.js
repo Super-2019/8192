4fc8a5e5219a50f0fb7ec590b56ad43c9f1db366
Super-2019 <78669085+Super-2019@users.noreply.github.com>
2022-07-14 08:39:33 -0400

    Update application.js

--- js/application.js
+++ js/application.js
@@ -1,4 +1,6 @@
 // Wait till the browser is ready to render the game (avoids glitches)
+version = "2"
+console.error("Version: " + version + " Updated?")
 window.requestAnimationFrame(function () {
   new GameManager(5, 8192, 4, KeyboardInputManager, HTMLActuator, LocalScoreManager);
 });
